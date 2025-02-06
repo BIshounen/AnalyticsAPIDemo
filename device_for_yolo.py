83eb54b2d815f5165642bf2a2f14178fcd415cee
vikochnev <113198688+vikochnev@users.noreply.github.com>
2025-02-06 23:14:14 +0300

    Changed logic of device priority that it will fallback in following order: cuda->mps->cpu

--- device_for_yolo.py
+++ device_for_yolo.py
@@ -1,12 +1,10 @@
-import platform
+import torch.cuda
 
 
 def get_device_name() -> str:
-    platform_name = platform.system()
-    match platform_name:
-        case 'Windows':
-            return 'cpu'
-        case 'Linux' | 'Darwin':
-            return 'mps'
-        case _:
-            raise Exception("Unexpected platform name")
+    if torch.cuda.is_available():
+        return 'cuda'
+    elif torch.mps.is_available():
+        return 'mps'
+    else:
+        return 'cpu'
